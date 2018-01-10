35143f5acaf9937c219b182c0d0b175912ca7e60
Unknown <cj@sportspass.com.au>
2018-01-10 09:27:45 +0800

    Gentellela Admin

---
+++ admin/app/config/app.js
@@ -0,0 +1,48 @@
+
+(function(){
+
+	angular.module('clusterMgmtApp', [
+		// 'app.localdev.config',
+		'ui.router'
+	])
+
+	.config(function ($stateProvider, $urlRouterProvider, $httpProvider) {
+
+		$urlRouterProvider
+	    .when('', '/dashboard')
+
+      $stateProvider
+          .state('dashboard', {
+        url: '/dashboard',
+              views: {
+                '@': {
+                  templateUrl: 'app/layout/dashboard.html'
+                },
+                'header@dashboard' : {
+                  templateUrl: 'app/layout/header.html'
+                },
+                'content@dashboard' : {
+                  templateUrl: 'app/layout/content_home.html'
+                },
+                'sidebar@dashboard' : {
+                  templateUrl: 'app/layout/sidebar.html'
+                },
+                'footer@dashboard' : {
+                  templateUrl: 'app/layout/footer.html'
+                }
+              },
+            });
+						// use the HTML5 History API
+			 $httpProvider.html5Mode(true);
+	})
+	.run(function ($rootScope, $state, $location) {
+		//console.log($location);
+
+		$rootScope.$on('$viewContentLoaded', function(event){
+
+	    });
+		$rootScope.$on('$stateChangeSuccess', function (event, toState, toParams, fromState, fromParams) {
+
+      	});
+	});
+})();
